c5dea8233def88526c55fe7e4784e2ba92c5a3ca
Katie Byers <lobsterkatie@gmail.com>
2017-12-06 14:50:17 -0800

    part 1 day 6 working

---
+++ dec6.py
@@ -0,0 +1,54 @@
+def find_length_of_infinite_loop(initial_allocation_str):
+    """Given the number of blocks in each memory location, figure out how many
+       reallocation cycles it takes before the reallocation enters an infinite
+       loop."""
+
+
+    #create a list to hold the number of blocks currently in each memory
+    #location, and a set to hold strings representing every state we've seen
+    #before
+    current_allocation = map(int, initial_allocation_str.split())
+    seen_allocations = set([initial_allocation_str])
+
+    #figure out how many locations we have (should be 16 in this problem)
+    num_locations = len(current_allocation)
+
+    #keep track of how many cycles we run through
+    num_cycles = 0
+
+    while True:
+
+        #find the location with the most blocks (lower indices break ties)
+        max_blocks = max(current_allocation)
+        loc_of_max = current_allocation.index(max_blocks)
+
+        #clear out that location
+        current_allocation[loc_of_max] = 0
+
+        #redistribute the blocks
+        for i in range(1, max_blocks + 1):
+            #start in the next location and loop around to the beginning,
+            #adding a block at a time
+            current_allocation[(loc_of_max + i) % num_locations] += 1
+
+        #update cycle tally
+        num_cycles += 1
+
+        #stringify the current allocation so we can check if we've seen it
+        #before
+        allocation_str = " ".join(map(str, current_allocation))
+
+        #if we have, stop
+        if allocation_str in seen_allocations:
+            break
+        #otherwise, store the new allocation for posterity
+        else:
+            seen_allocations.add(allocation_str)
+
+    #return the result
+    return num_cycles
+
+
+print find_length_of_infinite_loop("0 2 7 0")
+print find_length_of_infinite_loop("14 0 15 12 11 11 3 5 1 6 8 4 9 1 8 4")
+
