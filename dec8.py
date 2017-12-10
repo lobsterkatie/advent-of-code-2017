b7e2aaa4b719ec9d6a7df9ca5e7b0668537ba1a0
Katie Byers <lobsterkatie@gmail.com>
2017-12-09 20:31:26 -0800

    added instructions

--- dec8.py
+++ dec8.py
@@ -1,7 +1,58 @@
+"""--- Day 8: I Heard You Like Registers ---
+
+You receive a signal directly from the CPU. Because of your recent assistance
+with jump instructions, it would like you to compute the result of a series of
+unusual register instructions.
+
+Each instruction consists of several parts: the register to modify, whether to
+increase or decrease that register's value, the amount by which to increase or
+decrease it, and a condition. If the condition fails, skip the instruction
+without modifying the register. The registers all start at 0. The instructions
+look like this:
+
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+
+These instructions would be processed as follows:
+
+    Because a starts at 0, it is not greater than 1, and so b is not modified.
+
+    a is increased by 1 (to 1) because b is less than 5 (it is 0).
+
+    c is decreased by -10 (to 10) because a is now greater than or equal to 1
+    (it is 1).
+
+    c is increased by -20 (to -10) because c is equal to 10.
+
+After this process, the largest value in any register is 1.
+
+You might also encounter <= (less than or equal to) or != (not equal to).
+However, the CPU doesn't have the bandwidth to tell you what all the registers
+are named, and leaves that to you to determine.
+
+What is the largest value in any register after completing the instructions in
+your puzzle input?
+
+Your puzzle answer was 5102.
+
+
+--- Part Two ---
+
+To be safe, the CPU also needs to know the highest value held in any register
+during this process so that it can decide how much memory to allocate to these
+operations. For example, in the above instructions, the highest value ever
+held was 10 (in register c after the third instruction was evaluated).
+
+Your puzzle answer was 6056.
+
+"""
+
 from collections import defaultdict
 
 
-def find_largest_register_value(instructions):
+def find_largest_register_values(instructions):
     """Given a series of instructions for adjusting register values, determine
        the greatest register value after all instructions have been followed.
     """
@@ -9,6 +60,9 @@ def find_largest_register_value(instructions):
     #create the registers (since int() = 0, any register which doesn't exist
     #yet will be given an initial value of 0)
     registers = defaultdict(int)
+
+    #keep track of the highest value seen at any point in the process
+    max_ever = 0
 
     #run through the instructions, executing each one
     for instruction in instructions:
@@ -32,12 +86,19 @@ def find_largest_register_value(instructions):
             #make the change
             registers[register_to_change] += value_delta
 
-    #return the max register value
-    return max(registers.itervalues())
+            #update our max_ever if necessary
+            if registers[register_to_change] > max_ever:
+                max_ever = registers[register_to_change]
+
+    #return the max register value at the end, as well as the max value ever
+    end_max = max(registers.itervalues())
+    return end_max, max_ever
 
 
 
 
 
 with open("dec8.txt") as input_file:
-    print find_largest_register_value(input_file.readlines())
+    end_max, max_ever = find_largest_register_values(input_file.readlines())
+    print "end max:", end_max
+    print "max ever:", max_ever
